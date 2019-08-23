569d918d4ac02f21931f4de82ef39682180c5034
Matthew J. Hughes <mjhughes707@gmail.com>
2019-08-23 14:56:03 -0700

    resolved slow down issue (note to self: remember to add beginPath())

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 function drawTV() {
   // draws the television
+  ctx.beginPath();
   ctx.fillStyle = "rgb(33, 33, 33)"; // inner TV
   ctx.rect(0, 0, width, height);
   ctx.fill();
@@ -10,7 +11,7 @@ function drawTV() {
   ctx.stroke();
 }
 
-function drawRect() {
+function drawCube() {
   ctx.fillRect(cube.px - cube.radius/2, cube.py - cube.radius/2, cube.radius, cube.radius);
 }
 
@@ -32,51 +33,51 @@ function addFont() {
 function drawRed() {
   // draws the red square
   ctx.fillStyle = "red";
-  drawRect();
+  drawCube();
   addFont();
 }
 
 function drawBlue() {
   // draws the blue square
   ctx.fillStyle = "blue";
-  drawRect();
+  drawCube();
   addFont();
 }
 
 function drawGreen() {
   // draws the green square
   ctx.fillStyle = "limegreen";
-  drawRect();
+  drawCube();
   addFont();
 }
 
 function mainLoop() {
+  window.requestAnimationFrame(mainLoop);
   ctx.clearRect(0, 0, width, height); // eliminates "image drag"
   drawTV();
   moveCube();
   drawRed();
-  window.requestAnimationFrame(mainLoop);
 }
 
 function moveCube() {
   cube.px += cube.vx; // horizontal speed based on velocity
   cube.py += cube.vy; // vertical speed based on velocity
   // left border
-  if (cube.px - cube.radius < 0) {
-    cube.px = cube.radius + cube.movement;
+  if (cube.px - cube.radius/2 < tvBorder/2) {
+    cube.px = tvBorder/2 + cube.radius/2;
     cube.vx *= -1;
   // right border
-  } else if (cube.px + cube.radius > width) {
-    cube.px = width - cube.radius - cube.movement;
+  } else if (cube.px + cube.radius/2 > (width - tvBorder/2)) {
+    cube.px = (width - tvBorder/2) - cube.radius/2;
     cube.vx *= -1;
   }
   // top border
-  if (cube.py - cube.radius < 0) {
-    cube.py = cube.radius + cube.movement;
+  if (cube.py - cube.radius/2 < tvBorder/2) {
+    cube.py = tvBorder/2 + cube.radius/2;
     cube.vy *= -1;
   // bottom border
-  } else if (cube.py + cube.radius > height) {
-    cube.py = height - cube.radius - cube.movement;
+  } else if (cube.py + cube.radius/2 > (height - tvBorder/2)) {
+    cube.py = (height - tvBorder/2) - cube.radius/2;
     cube.vy *= -1;
   }
 }
@@ -90,20 +91,17 @@ let ctx = cnv.getContext("2d");
 
 // sets canvas size variables for use in functions
 // (800:600 ratio + outside border)
-let width = 966;
-let height = 766;
-let tvBorder = 83;
+let width = 960;
+let height = 760;
+let tvBorder = 80;
 
 // sets cube size, random position, velocity and movement
 let cube = {
-  radius: 135,
-  px: width/2,
-  py: height/2,
-  // px: Math.random() * width, // cube's x position
-  // py: Math.random() * height, // cube's y position
-  vx: 5, // cube's x velocity
-  vy: 5, // cube's y velocity
-  movement: .001
+  radius: 130,
+  px: Math.random() * width, // cube's x position
+  py: Math.random() * height, // cube's y position
+  vx: 3, // cube's x velocity
+  vy: 3, // cube's y velocity
 };
 
 window.requestAnimationFrame(mainLoop); // enters loop
