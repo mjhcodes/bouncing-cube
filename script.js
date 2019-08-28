c41b76895dfabc7a248731b92fa4e1117a017b77
Matthew J. Hughes <mjhughes707@gmail.com>
2019-08-27 20:27:16 -0700

    reduced size of TV, centered TV, changed color of cube on bounce

--- script.js
+++ script.js
@@ -12,51 +12,24 @@ function drawTV() {
 }
 
 function drawCube() {
+  // draws the square of the cube with the current color
+  ctx.fillStyle = cube.colors[c];
   ctx.fillRect(cube.px - cube.radius/2, cube.py - cube.radius/2, cube.radius, cube.radius);
 }
 
 function addFont() {
   // adds the DVD font
   ctx.fillStyle = "black";
-  ctx.font = "50px Impact";
+  ctx.font = "40px Impact";
   let topText = "DVD";
   let topTextWidth = ctx.measureText(topText).width;
   ctx.fillText(topText, cube.px - topTextWidth/2, cube.py);
   // adds the video font
   ctx.fillStyle = "white";
-  ctx.font = "29px Impact";
+  ctx.font = "22px Impact";
   let bottomText = "VIDEO";
   let bottomTextWidth = ctx.measureText(bottomText).width;
   ctx.fillText(bottomText, cube.px - bottomTextWidth/2, cube.py + (cube.radius/4));
-}
-
-function drawRed() {
-  // draws the red square
-  ctx.fillStyle = "red";
-  drawCube();
-  addFont();
-}
-
-function drawBlue() {
-  // draws the blue square
-  ctx.fillStyle = "blue";
-  drawCube();
-  addFont();
-}
-
-function drawGreen() {
-  // draws the green square
-  ctx.fillStyle = "limegreen";
-  drawCube();
-  addFont();
-}
-
-function mainLoop() {
-  window.requestAnimationFrame(mainLoop);
-  ctx.clearRect(0, 0, width, height); // eliminates "image drag"
-  drawTV();
-  moveCube();
-  drawRed();
 }
 
 function moveCube() {
@@ -66,20 +39,33 @@ function moveCube() {
   if (cube.px - cube.radius/2 < tvBorder/2) {
     cube.px = tvBorder/2 + cube.radius/2;
     cube.vx *= -1;
+    if ( c < 2 ) { c += 1; } else if ( c == 2 ) { c = 0; } // cycles color
   // right border
   } else if (cube.px + cube.radius/2 > (width - tvBorder/2)) {
     cube.px = (width - tvBorder/2) - cube.radius/2;
     cube.vx *= -1;
+    if ( c < 2 ) { c += 1; } else if ( c == 2 ) { c = 0; } // cycles color
   }
   // top border
   if (cube.py - cube.radius/2 < tvBorder/2) {
     cube.py = tvBorder/2 + cube.radius/2;
     cube.vy *= -1;
+    if ( c < 2 ) { c += 1; } else if ( c == 2 ) { c = 0; } // cycles color
   // bottom border
   } else if (cube.py + cube.radius/2 > (height - tvBorder/2)) {
     cube.py = (height - tvBorder/2) - cube.radius/2;
     cube.vy *= -1;
+    if ( c < 2 ) { c += 1; } else if ( c == 2 ) { c = 0; } // cycles color
   }
+}
+
+function mainLoop() {
+  window.requestAnimationFrame(mainLoop);
+  ctx.clearRect(0, 0, width, height); // eliminates "image drag"
+  drawTV();
+  moveCube();
+  drawCube();
+  addFont();
 }
 
 /***********************************************/
@@ -90,18 +76,19 @@ let cnv = document.getElementById("cnv");
 let ctx = cnv.getContext("2d");
 
 // sets canvas size variables for use in functions
-// (800:600 ratio + outside border)
-let width = 960;
-let height = 760;
-let tvBorder = 80;
+let width = 800;
+let height = 600;
+let tvBorder = 100;
+let c = 0;
 
 // sets cube size, random position, velocity and movement
 let cube = {
-  radius: 130,
+  colors: ["red", "limegreen", "blue"],
+  radius: 100,
   px: Math.random() * width, // cube's x position
   py: Math.random() * height, // cube's y position
-  vx: 3, // cube's x velocity
-  vy: 3, // cube's y velocity
+  vx: 1.5, // cube's x velocity
+  vy: 1.5, // cube's y velocity
 };
 
 window.requestAnimationFrame(mainLoop); // enters loop
